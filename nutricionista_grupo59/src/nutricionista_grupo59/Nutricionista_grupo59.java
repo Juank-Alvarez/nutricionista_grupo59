6f84944bcc84d090b702d9c42970b6fa9d27fe31
GutsMaciel <gustavo_02_16@hotmail.com>
2023-10-03 17:55:11 -0300

    probando busqueda

--- nutricionista_grupo59/src/nutricionista_grupo59/Nutricionista_grupo59.java
+++ nutricionista_grupo59/src/nutricionista_grupo59/Nutricionista_grupo59.java
@@ -9,19 +9,19 @@ import java.util.ArrayList;
 public class Nutricionista_grupo59 {
 
     public static void main(String[] args) {
-//        Paciente paciente1=new Paciente("Mauricio","Herrera",12345567,"SanJuan 2045","32435632",3,true);
+//        Paciente paciente1=new Paciente("Mauricio","Herrera",12345567,"SanJuan 2045","435632",3,true);
         PacienteData pd=new PacienteData();
 //        pd.agregarPaciente(paciente1);
 //        pd.modificarPaciente(paciente1);
 //        pd.eliminarPaciente(3);
-         ArrayList<Paciente> lista=new ArrayList<>();
-         lista=(ArrayList<Paciente>) pd.listarPacientes();
-         for(Paciente pac : lista){
-             
-             System.out.println(pac.toString());
-         }
-         
-        
+//         ArrayList<Paciente> lista=new ArrayList<>();
+//         lista=(ArrayList<Paciente>) pd.listarPacientes();
+//         for(Paciente pac : lista){
+//             System.out.println(pac.toString());
+//         }
+        Paciente pac=new Paciente();
+         pac=pd.buscarPaciente(3);
+        System.out.println(pac.toString());
     }
     
 }
